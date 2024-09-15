333c3a0bf85546750905adf0aa07a2661a697e1d
shash0423 <shash042319@naver.com>
2024-09-15 09:00:03 +0900

    초기 세팅

---
+++ week2/src/main/java/com/pironeer/week2/repository/TopicRepository.java
@@ -0,0 +1,24 @@
+package com.pironeer.week2.repository;
+
+import com.pironeer.week2.repository.domain.Topic;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Repository
+public class TopicRepository {
+    private final AtomicLong topicIdxGenerator = new AtomicLong(0);
+    private final Map<Long, Topic> topicMap = new HashMap<Long, Topic>();
+
+    public void save(Topic topic) {
+        Long id = topicIdxGenerator.getAndIncrement();
+        topic.setId(id);
+        topicMap.put(topicIdxGenerator.getAndIncrement(), topic);
+    }
+
+    public Topic findById(Long id) {
+        return topicMap.get(id);
+    }
+}
---
+++ week2/src/main/java/com/pironeer/week2/service/TopicService.java
@@ -0,0 +1,34 @@
+package com.pironeer.week2.service;
+
+import com.pironeer.week2.dto.request.TopicCreateRequest;
+import com.pironeer.week2.dto.response.TopicResponse;
+import com.pironeer.week2.repository.TopicRepository;
+import com.pironeer.week2.repository.domain.Topic;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cglib.core.Local;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class TopicService {
+
+    private final TopicRepository topicRepository;
+
+    public void save(TopicCreateRequest request) {
+        Topic topic = Topic.builder()
+                .title(request.title())
+                .content(request.content())
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        topicRepository.save(topic);
+    }
+
+    public TopicResponse findById(Long id) {
+        Topic topic = topicRepository.findById(id);
+        return TopicResponse.of(topic);
+    }
+}
