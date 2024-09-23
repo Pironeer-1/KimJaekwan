0f990cd81af5f488402830e6c441fc33d96ec62c
shash0423 <shash042319@naver.com>
2024-09-23 20:38:00 +0900

    feat: Member 목록 조회

--- week3/src/main/java/pironeer/week3/member/controller/MemberController.java
+++ week3/src/main/java/pironeer/week3/member/controller/MemberController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import pironeer.week3.global.dto.response.SuccessResponse;
+import pironeer.week3.global.dto.response.result.ListResult;
 import pironeer.week3.global.dto.response.result.SingleResult;
 import pironeer.week3.member.dto.request.MemberRequest;
 import pironeer.week3.member.dto.response.MemberResponse;
@@ -30,6 +31,13 @@ public class MemberController {
     @Operation(summary = "회원 단건 조회")
     public SuccessResponse<SingleResult<MemberResponse>> readMember(@PathVariable("memberId") Long id) {
         SingleResult<MemberResponse> result = memberService.findMemberById(id);
+        return SuccessResponse.ok(result);
+    }
+
+    @GetMapping
+    @Operation(summary = "회원 목록 조회")
+    public SuccessResponse<ListResult<MemberResponse>> readMembers() {
+        ListResult<MemberResponse> result = memberService.findMembers();
         return SuccessResponse.ok(result);
     }
 }
--- week3/src/main/java/pironeer/week3/member/service/MemberService.java
+++ week3/src/main/java/pironeer/week3/member/service/MemberService.java
@@ -2,6 +2,8 @@ package pironeer.week3.member.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import pironeer.week3.global.dto.response.SuccessResponse;
+import pironeer.week3.global.dto.response.result.ListResult;
 import pironeer.week3.global.dto.response.result.SingleResult;
 import pironeer.week3.global.service.ResponseService;
 import pironeer.week3.member.dto.request.MemberRequest;
@@ -10,7 +12,8 @@ import pironeer.week3.member.entity.Member;
 import pironeer.week3.member.mapper.MemberMapper;
 import pironeer.week3.member.repository.MemberRepository;
 
-import java.util.Optional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -27,5 +30,15 @@ public class MemberService {
         MemberResponse response = MemberResponse.of(member);
 
         return ResponseService.getSingleResult(response);
+    }
+
+    public ListResult<MemberResponse> findMembers() {
+        List<Member> members = memberRepository.findAll();
+
+        List<MemberResponse> response = members.stream()
+                .map(member -> MemberResponse.of(member))
+                .collect(Collectors.toList());
+
+        return ResponseService.getListResult(response);
     }
 }
