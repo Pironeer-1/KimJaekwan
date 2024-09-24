d589860d5b3a8e422180c6b63cf7e9b16edb60ed
shash0423 <shash042319@naver.com>
2024-09-24 13:47:21 +0900

    로그인시 response 헤더에 jwt 토큰 값 반환되도록 구현

--- week3/src/main/java/pironeer/week3/global/config/SecurityConfig.java
+++ week3/src/main/java/pironeer/week3/global/config/SecurityConfig.java
@@ -1,25 +1,42 @@
 package pironeer.week3.global.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import pironeer.week3.global.jwt.JWTUtil;
+import pironeer.week3.global.jwt.LoginFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
 
     // 인증이 필요하지 않은 URL 목록
     private final String[] allowedUrls = {
+            "/",
             "/api/**", // 임시 추가
             "/api/members/login",
             "/api/members/register",
             "/swagger-ui/**",
             "/v3/api-docs/**",
     };
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
@@ -49,6 +66,10 @@ public class SecurityConfig {
                         .requestMatchers("admin").hasRole("ADMIN")
                         .anyRequest().authenticated()
                         );
+
+        // 필터 추가
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
         // 세션 설정
         http
---
+++ week3/src/main/java/pironeer/week3/member/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package pironeer.week3.member.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pironeer.week3.global.exception.CustomException;
+import pironeer.week3.global.exception.ErrorCode;
+import pironeer.week3.member.dto.CustomUserDetails;
+import pironeer.week3.member.entity.Member;
+import pironeer.week3.member.repository.MemberRepository;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+
+        Member member = memberRepository.findByUsername(username)
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_EXIST));
+
+        return new CustomUserDetails(member);
+    }
+}
